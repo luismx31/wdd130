4e76a5681e282fc9c26d28dc9b0c7c37b5022e97
luismx31 <luiselmx22@gmail.com>
2024-05-27 23:31:00 -0400

    quiz.html

--- wwr/styles/week4.py
+++
@@ -1,8 +0,0 @@
-number = int(input("Please type a positive number:"))
-
-while number < 0:
-    print("sorry, that is a negative umber. please try again.")
-    number = int(input("Please type a positive number:"))
-    
-
-    print(f"The number is {number}")
